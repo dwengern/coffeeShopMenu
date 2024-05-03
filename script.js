14fe61a3a8f03b9c35862a9d8964ca325474df4e
Nick Dwenger <ndwenger624@gmail.com>
2024-05-03 13:39:26 -0400

    Added inital menu content and items

---
+++ script.js
@@ -0,0 +1,59 @@
+function img_expresso(){
+    const el = document.getElementById("menu_image");
+    el.src = "https://th.bing.com/th?id=OIP.-fN7-Y36OOQxV-zlueT-pgHaGQ&w=272&h=229&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2";
+}
+
+function img_capp(){
+    const el = document.getElementById("menu_image");
+    el.src = "https://th.bing.com/th?id=OIP.g108wW-nRM0B_tqRJa6wJgHaE8&w=306&h=204&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2";
+}
+
+function img_latte(){
+    const el = document.getElementById("menu_image");
+    el.src = "https://th.bing.com/th/id/OIP.QHR6qPxWInhwUXVyGtTS0QHaGk?w=203&h=180&c=7&r=0&o=5&pid=1.7";
+}
+
+function img_croissant(){
+    const el = document.getElementById("menu_image");
+    el.src = "https://th.bing.com/th/id/OIP.hfGfb4bACzwkFIm8QWk79wHaHa?w=181&h=181&c=7&r=0&o=5&pid=1.7";
+}
+
+function img_muffin(){
+    const el = document.getElementById("menu_image");
+    el.src = "https://th.bing.com/th/id/OIP.ZeK01AedSSWM4_hm1W4ofwHaHa?w=184&h=184&c=7&r=0&o=5&pid=1.7";
+}
+
+function img_cookie(){
+    const el = document.getElementById("menu_image");
+    el.src = "https://th.bing.com/th/id/OIP.CHm9a57449DBIeoHJPQt_wHaEK?w=285&h=180&c=7&r=0&o=5&pid=1.7";
+}
+
+function bold_expresso(){
+    const el = document.getElementById("expresso");
+    el.classList.toggle("bold");
+}
+
+function bold_capp(){
+    const el = document.getElementById("cappuccino");
+    el.classList.toggle("bold");
+}
+
+function bold_latte(){
+    const el = document.getElementById("latte");
+    el.classList.toggle("bold");
+}
+
+function bold_croissant(){
+    const el = document.getElementById("croissant");
+    el.classList.toggle("bold");
+}
+
+function bold_muffin(){
+    const el = document.getElementById("muffin");
+    el.classList.toggle("bold");
+}
+
+function bold_cookie(){
+    const el = document.getElementById("cookie");
+    el.classList.toggle("bold");
+}
